04a0fcf4b04fc5bfe5aa1568649de07b318c3409
Alexander_Kononov1@epam.com <alexander_kononov1@epam.com>
2021-12-14 23:29:46 +0300

    refactoring

--- EveryDayJogging.py
+++ EveryDayJogging.py
@@ -20,7 +20,7 @@ day_counter = 1
 
 while daily_result <= final_target:
     daily_result += daily_result / 100 * RESULT_ENHANCING
-    day_counter +=   day_counter + 1.
+    day_counter += 1
     # print(f"{day_counter}-й день: {a:.2f}")
 
 print(f"Спортсмен достиг результата на {int(day_counter)} день")
