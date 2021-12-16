38b3f15a62c13ce1029ae518aae0ac50c66454d7
Alexander_Kononov1@epam.com <alexander_kononov1@epam.com>
2021-12-16 19:52:40 +0300

    created second HW

--- TripleNSum.py
+++
@@ -1,8 +0,0 @@
-# 3. Узнайте у пользователя число n. Найдите сумму чисел n + nn + nnn. Например, пользователь ввёл число 3.
-# Считаем 3 + 33 + 333 = 369.
-
-num_input = int(input("Введите число n: "))
-
-sum_n = num_input + int(f"{num_input}" * 2) + int(f"{num_input}" * 3)
-
-print(f"Сумма {num_input} + {num_input}{num_input} + {num_input}{num_input}{num_input} равна: {sum_n}")
