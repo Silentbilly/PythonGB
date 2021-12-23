6ae88fd7e1f35cb1b999638aaee53aaefc3c3365
Alexander_Kononov1@epam.com <alexander_kononov1@epam.com>
2021-12-23 22:17:42 +0300

    refactoring

--- salary_counting.py
+++ salary_counting.py
@@ -3,9 +3,12 @@
 # для конкретных значений необходимо запускать скрипт с параметрами.
 from sys import argv
 
-script_name, output_per_hour, wage_per_hour, bonus = argv
+try:
+    script_name, output_per_hour, wage_per_hour, bonus = argv
 
-salary = int(output_per_hour) * int(wage_per_hour) + int(bonus)
+    salary = int(output_per_hour) * int(wage_per_hour) + int(bonus)
 
-print(f"Зарплата сотрудника при выработке в часах = {output_per_hour}$, ставке в час = {wage_per_hour}$ + "
-      f"премия {bonus}$: {salary}$")
+    print(f"Зарплата сотрудника при выработке в часах = {output_per_hour}$, ставке в час = {wage_per_hour}$ + "
+          f"премия {bonus}$: {salary}$")
+except ValueError as err:
+    print(err)
