e9b8458681805b8e05f8c1b78d57389b19ea7a85
Alexander_Kononov1@epam.com <alexander_kononov1@epam.com>
2021-12-27 19:38:14 +0300

    added 5 HW

---
+++ CreateFileByProgram.py
@@ -0,0 +1,8 @@
+# 1. Создать программно файл в текстовом формате, записать в него построчно данные, вводимые пользователем.
+# Об окончании ввода данных свидетельствует пустая строка.
+
+my_file = open("text_1.txt", "w+", encoding="utf-8")
+input_text = " "
+while input_text != "":
+    input_text = input(">/")
+    my_file.write(input_text + "\n") if input_text != "" else my_file.write(input_text)
